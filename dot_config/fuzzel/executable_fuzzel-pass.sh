647850726dc490bde64fa0cacfbf129238391486
Simon Rieger <rieger@netzlink.com>
2023-05-04 13:55:56 +0200

    config for fuzzel

--- dot_config/fuzzel/executable_fuzzel-pass.sh
+++ dot_config/fuzzel/executable_fuzzel-pass.sh
@@ -2,6 +2,6 @@
 # Use fuzzel to find a password from password-store and copy it to clipboard.
 # inpired by: https://gist.github.com/igemnace/2b8609d280752e8a1b173204c14f6892
 
-pass_name=$(cd $HOME/.password-store && rg --files | sed 's/\.gpg$//' | fuzzel -d -w 60 -D yes)
+pass_name=$(cd $HOME/.password-store && rg --files | sed 's/\.gpg$//' | fuzzel -d)
 
 [[ $pass_name != "" ]] && pass show -c $pass_name
