4b3b9172a10192f5967f0fc4dc94fb94ce6ea1ee
Simon Rieger <simono41@brothertec.eu>
2023-06-22 18:06:31 +0200

    add rofi

---
+++ dot_config/rofi/executable_rofi-pass.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Use fuzzel to find a password from password-store and copy it to clipboard.
+# inpired by: https://gist.github.com/igemnace/2b8609d280752e8a1b173204c14f6892
+
+pass_name=$(cd $HOME/.password-store && rg --files | sed 's/\.gpg$//' | rofi -dmenu)
+
+[[ $pass_name != "" ]] && pass show -c $pass_name
