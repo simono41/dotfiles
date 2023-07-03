813195bed4ccbccff9a002049bb001d445947642
Rieger, Simon <rieger@netzlink.com>
2023-07-03 09:33:03 +0200

    fix khal

--- dot_config/waybar/executable_waybar-khal.py
+++ dot_config/waybar/executable_waybar-khal.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python
+
 import json
 import subprocess
+from html import escape
+
 data = {}
+
 output = subprocess.check_output("khal list now 7days --format \"{start-end-time-style} {title}\"", shell=True).decode("utf-8")
+
 lines = output.split("\n")
 new_lines = []
 for line in lines:
-    if len(line) and line[0].isalpha():
-        line = "\n<b>"+line+"</b>"
-    new_lines.append(line)
+    clean_line = escape(line).split(" ::")[0]
+    if len(clean_line) and not clean_line[0] in ['0', '1', '2']:
+        clean_line = "\n<b>"+clean_line+"</b>"
+    new_lines.append(clean_line)
 output = "\n".join(new_lines).strip()
+
 if "Today" in output:
     data['text'] = " " + output.split('\n')[1]
 else:
     data['text'] = ""
+
 data['tooltip'] = output
+
 print(json.dumps(data))
