551dc964fe2c99c2ee6989a0eca72d00899017f7
Simon Rieger <simono41@brothertec.eu>
2023-08-29 01:00:46 +0200

    fix choose-clip for unknown data type for json

--- dot_config/choose/executable_choose-clip.sh
+++ dot_config/choose/executable_choose-clip.sh
@@ -2,6 +2,6 @@
 # Use fuzzel to find a password from password-store and copy it to clipboard.
 # inpired by: https://gist.github.com/igemnace/2b8609d280752e8a1b173204c14f6892
 
-clip_name=$(cd $HOME/Library/Containers/com.generalarcade.flycut/Data/Library/Preferences && plutil -convert json com.generalarcade.flycut.plist && cat com.generalarcade.flycut.plist | jq '.store.jcList[] | {Contents} | join(" ")' | sed -e 's/[^.]//;s/.$//' | choose)
+clip_name=$(cd $HOME/Library/Containers/com.generalarcade.flycut/Data/Library/Preferences && plutil -extract store xml1 com.generalarcade.flycut.plist -o - | plutil -convert json -o - - | jq '.jcList[] | {Contents} | join(" ")' | sed -e 's/[^.]//;s/.$//' | choose)
 
 [[ $clip_name != "" ]] && echo $clip_name | pbcopy
