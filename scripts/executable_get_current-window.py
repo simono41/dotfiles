4f05f2bd20a57f1c2be2886b7c30a535a57057f5
Simon Rieger <simono41@brothertec.eu>
2023-08-30 14:19:05 +0200

    changes for macos

--- scripts/executable_get_current-window.py
+++ scripts/executable_get_current-window.py
@@ -11,6 +11,7 @@ if sys.platform == "darwin":
     )
 
 if sys.platform == "darwin":
+    get_this = sys.argv[1]
     curr_app = NSWorkspace.sharedWorkspace().frontmostApplication()
     curr_pid = NSWorkspace.sharedWorkspace().activeApplication()['NSApplicationProcessIdentifier']
     curr_app_name = curr_app.localizedName()
@@ -23,6 +24,13 @@ if sys.platform == "darwin":
         geometry = window['kCGWindowBounds']
         windowTitle = window.get('kCGWindowName', u'Unknown')
         if curr_pid == pid:
-            print("%s - %s (PID: %d, WID: %d): %s" % (ownerName, windowTitle.encode('ascii','ignore'), pid, windowNumber, geometry))
+            if get_this == "all":
+                print("%s - %s (PID: %d, WID: %d): %s" % (
+                ownerName, windowTitle.encode('ascii', 'ignore'), pid, windowNumber, geometry))
+            elif get_this == "pid":
+                print(pid)
+            elif get_this == "title":
+                print(ownerName)
 elif sys.platform == "win32":
     (active_app_name, windowTitle) = _getActiveInfo_Win32()
+
