8d4edd45c08ce9554a8d9b78412109ff76cf026f
simono41 <simono41@brothertec.eu>
2025-04-17 18:42:09 +0200

    executable_install-macos.sh aktualisiert

--- executable_install-macos.sh
+++ executable_install-macos.sh
@@ -43,6 +43,7 @@ brew install kdoctor cocoapods
 # Optionale Tools installieren (Visual Studio Code, Prism Launcher)
 brew install --cask visual-studio-code prismlauncher
 brew install spotify_player
+brew install minio/stable/mc
 
 # RBW (Rust Bitwarden CLI) Installation und Konfiguration
 brew install rbw
