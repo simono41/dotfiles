907a94ede0e248c0c06637334012c59e32cd7ea5
Simon Rieger <simon@rieger.app>
2025-04-22 09:13:57 +0200

    change notification system to swaync

--- executable_install-fedora.sh
+++ executable_install-fedora.sh
@@ -38,7 +38,7 @@ sudo dnf install -y \
   dkms libdrm-devel hplip ydotool pkg-config \
   libxkbcommon-devel scdoc inotify-tools \
   net-snmp-utils net-snmp snmpd virt-manager \
-  @virtualization virt-what mako
+  @virtualization virt-what SwayNotificationCenter
 
 ## Multimedia
 sudo dnf install -y \
