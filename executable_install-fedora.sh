b0ab971da10cceefd1db9191edd84379f16ca166
Simon Rieger <simono41@brothertec.eu>
2025-05-15 17:31:24 +0200

    update package installation script on arm

--- executable_install-fedora.sh
+++ executable_install-fedora.sh
@@ -132,7 +132,7 @@ else
     echo "$PACKAGE ist nicht installiert. Starte Download und Installation..."
     cd /tmp
     wget https://github.com/mahmoudbahaa/outlook-for-linux/releases/download/v1.3.14-outlook/outlook-for-linux-1.3.14.$(uname -m).rpm
-    sudo rpm -ivh outlook-for-linux-1.3.14.x86_64.rpm
+    sudo rpm -ivh outlook-for-linux-1.3.14.$(uname -m).rpm
     cd
 fi
 
